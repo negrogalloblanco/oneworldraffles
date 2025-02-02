eea78ea5d2223cd1312f31840338f942131da56b
negrogalloblanco <negrogalloblanco@gmail.com>
2025-02-01 23:14:34 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,14 @@ export default function Home() {
     return (
         <div className="container">
             <Head>
-                <title>One World Raffle Presents
-                        Rifas Mundiales</title>
+                <title>One World Raffle Presents </title>
             </Head>
             <main>
-                <h1>Welcome to One World Raffle</h1>
+                <h1>ONE World Raffles</h1>
+                        Join global raffles and stand a chance to win amazing prizes.
+
         
-                <p>Buy tickets for a chance to win a Mercedes-Benz EQE 2025 or 100,000WLD!</p>
+                <p>1 WLD Coin gives you a ticket for a chance to win a Mercedes-Benz EQE 2025 or 100,000WLD!</p>
             </main>
         </div>
     );
